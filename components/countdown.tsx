5732821e54a58258acb54743ec1ee51aeb5c8357
mrklwnc <endranomarklewence@gmail.com>
2025-11-04 17:59:10 +0800

    refactor(countdown): swap font-size from text-[32px] to text-3xl

--- components/countdown.tsx
+++ components/countdown.tsx
@@ -7,19 +7,19 @@ export default function Countdown() {
   return (
     <div className="flex-1 flex items-center font-playfair-display leading-none">
       <div className="flex-1 py-4 grid gap-1 place-content-center text-center h-full bg-custom-primary lg:bg-custom-primary/80">
-        <p className="text-[32px] font-bold lg:text-6xl">{days}</p>
+        <p className="text-3xl font-bold lg:text-6xl">{days}</p>
         <span className="text-xs lg:text-2xl">DAYS</span>
       </div>
       <div className="flex-1 py-4 grid gap-1 place-content-center text-center h-full bg-custom-primary-2 lg:bg-custom-primary-2/80">
-        <p className="text-[32px] font-bold lg:text-6xl">{hours}</p>
+        <p className="text-3xl font-bold lg:text-6xl">{hours}</p>
         <span className="text-xs lg:text-2xl">HOURS</span>
       </div>
       <div className="flex-1 py-4 grid gap-1 place-content-center text-center h-full bg-custom-primary-3 lg:bg-custom-primary-3/80">
-        <p className="text-[32px] font-bold lg:text-6xl">{minutes}</p>
+        <p className="text-3xl font-bold lg:text-6xl">{minutes}</p>
         <span className="text-xs lg:text-2xl">MINS</span>
       </div>
       <div className="flex-1 py-4 grid gap-1 place-content-center text-center h-full bg-custom-primary-4 lg:bg-custom-primary-4/80">
-        <p className="text-[32px] font-bold lg:text-6xl">{seconds}</p>
+        <p className="text-3xl font-bold lg:text-6xl">{seconds}</p>
         <span className="text-xs lg:text-2xl">SECS</span>
       </div>
     </div>
