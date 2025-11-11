38bafd0f0abe34a1d461781bee4e1fcd6219f53f
mrklwnc <endranomarklewence@gmail.com>
2025-11-11 23:20:54 +0800

    refactor(page): styling of Location section

--- app/invite/page.tsx
+++ app/invite/page.tsx
@@ -1,5 +1,5 @@
 import Image from "next/image";
-
+import CoupleCard from "@/components/couple-card";
 import {
   LocationCard,
   LocationContent,
@@ -29,8 +29,7 @@ export default function Invitation() {
         />
       </section>
 
-      {/* TODO: Put Location and Wedding Timeline in a single section */}
-      <section className="p-4 lg:px-40 2xl:px-100 space-y-2 lg:space-y-4">
+      <section className="p-4 xl:py-4 flex flex-col gap-2 lg:gap-4 xl:gap-6 justify-center items-center">
         <LocationCard>
           <LocationImage>
             <Image
@@ -87,31 +86,8 @@ export default function Invitation() {
       </section>
 
       <WeddingTimeline />
+
+      <section className="min-h-screen"></section>
     </div>
   );
 }
-
-const CoupleCard = ({
-  url,
-  name,
-  description,
-}: {
-  url: string;
-  name: string;
-  description: string;
-}) => {
-  return (
-    <div className="bg-white w-full max-w-xs lg:max-w-lg p-1 md:p-2 lg:p-3 space-y-3">
-      <div className="relative aspect-square">
-        <Image src={url} alt={name} fill className="object-cover box" />
-
-        <div className="absolute inset-0 outline-1 -outline-offset-6 outline-custom-primary-4"></div>
-      </div>
-
-      <div className="uppercase text-center text-black text-sm lg:space-y-2 py-2 lg:py-6">
-        <p className="font-semibold md:text-2xl lg:text-3xl">{name}</p>
-        <span className="md:text-xl lg:text-2xl">{description}</span>
-      </div>
-    </div>
-  );
-};
