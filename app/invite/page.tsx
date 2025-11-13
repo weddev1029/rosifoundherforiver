d55d20d01e883fd46941f3043c6b5424a07c0c02
mrklwnc <endranomarklewence@gmail.com>
2025-11-13 23:07:29 +0800

    todo: love story section

--- app/invite/page.tsx
+++ app/invite/page.tsx
@@ -7,14 +7,13 @@ import {
 } from "@/components/location-card";
 import { Button } from "@/components/ui/button";
 import { Hero, WeddingTimeline } from "@/features";
+import { cn } from "@/lib/utils";
 import { heroImages } from "@/siteConfig";
 
 export default function Invitation() {
   return (
     <div className="min-h-screen font-playfair-display">
-      <main>
-        <Hero />
-      </main>
+      <Hero />
 
       <section className="flex items-center justify-center gap-5 bg-[url('/hero/hero-0.jpg')] px-4 py-22 lg:px-8 lg:py-24 bg-cover bg-center bg-fixed lg:gap-28">
         <CoupleCard
@@ -84,37 +83,84 @@ export default function Invitation() {
 
       <WeddingTimeline />
 
-      <section className="grid grid-cols-2 md:grid-cols-3 bg-custom-secondary-3 py-10">
-        <div className="col-span-2 space-y-3 flex flex-col items-center md:col-span-1 mb-6 md:mb-0 md:justify-center">
-          <div className="relative aspect-video w-[50px] xl:w-[78.75px]">
-            <Image
-              src={"/svg/butterfly-ornament.svg"}
-              alt="butterfly-ornament"
-              fill
-              className="object-contain"
-            />
+      <section className="p-8 lg:flex bg-custom-secondary-3 justify-center items-center lg:py-10 xl:py-12">
+        <div className="grid grid-cols-2 lg:grid-cols-3">
+          <div className="pb-8 lg:p-4 col-span-2 space-y-3 flex flex-col items-center lg:col-span-1 xl:p-8 md:justify-center">
+            <div className="relative aspect-video min-w-[50px] max-w-[78.75px]">
+              <Image
+                src={"/svg/butterfly-ornament.svg"}
+                alt="butterfly-ornament"
+                fill
+                className="object-contain"
+              />
+            </div>
+
+            <h4 className="text-2xl text-background xl:text-4xl">
+              The{" "}
+              <span className="font-great-vibes text-3xl xl:text-5xl">
+                Love
+              </span>{" "}
+              in Gallery
+            </h4>
           </div>
 
-          <h4 className="text-2xl text-background xl:text-4xl">
-            The{" "}
-            <span className="font-great-vibes text-3xl xl:text-5xl">Love</span>{" "}
-            in Gallery
-          </h4>
+          {heroImages.map((image) => (
+            <div key={image.id} className="relative w-full aspect-video">
+              <Image
+                src={image.src}
+                alt={image.alt}
+                fill
+                className="object-cover object-center"
+              />
+            </div>
+          ))}
+        </div>
+      </section>
+
+      <section className="min-h-screen p-8 relative">
+        <div className="absolute -top-12 left-0 w-[85.44px] h-[292.78px] aspect-auto">
+          <Image
+            src="/svg/left-fern-ornament.svg"
+            alt="fern"
+            fill
+            className="object-contain object-center"
+          />
+        </div>
+        <div className="absolute top-0 right-0 w-[85.44px] h-[292.78px] aspect-auto">
+          <Image
+            src="/svg/right-fern-ornament.svg"
+            alt="fern"
+            fill
+            className="object-contain object-center"
+          />
         </div>
 
-        {heroImages.map((image) => (
-          <div
-            key={image.id}
-            className="relative w-[188.82px] h-[125.4px] xl:w-[365.25px] xl:h-[242.56px]"
-          >
+        <div className="grid grid-cols-2 gap-x-4 gap-y-5 z-99">
+          <div className="relative w-full aspect-square">
             <Image
-              src={image.src}
-              alt={image.alt}
+              src="/hero/hero-0.jpg"
+              alt="hero-0"
               fill
-              className="object-contain"
+              className="object-cover object-center"
             />
           </div>
-        ))}
+          <div className="relative w-full aspect-square">
+            <Image
+              src="/hero/hero-1.jpg"
+              alt="hero-1"
+              fill
+              className="object-cover object-center"
+            />
+          </div>
+          <div className="col-span-2 relative w-full aspect-video">
+            <Image
+              src="/hero/hero-2.jpg"
+              alt="hero-2"
+              fill
+              className="object-cover object-center"
+            />
+          </div>
+        </div>
       </section>
     </div>
   );
