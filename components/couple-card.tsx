036f37790bf01d951959881be48edb049c861246
Mark Lewence Endrano <endranomarklewence@gmail.com>
2025-11-14 17:32:03 +0800

    refactor(couple-card): fixed height and weight

--- components/couple-card.tsx
+++ components/couple-card.tsx
@@ -10,7 +10,7 @@ export default function CoupleCard({
   description: string;
 }) {
   return (
-    <div className="bg-white w-full max-w-xs lg:max-w-lg p-1 md:p-2 lg:p-3 space-y-3">
+    <div className="bg-white w-[194.95px] md:w-56 lg:w-96 xl:w-[492px] p-1 md:p-2 lg:p-3 space-y-3">
       <div className="relative aspect-square">
         <Image src={url} alt={name} fill className="object-cover box" />
 
@@ -18,8 +18,8 @@ export default function CoupleCard({
       </div>
 
       <div className="uppercase text-center text-black text-sm lg:space-y-2 py-2 lg:py-6">
-        <p className="font-semibold md:text-2xl lg:text-3xl">{name}</p>
-        <span className="md:text-xl lg:text-2xl">{description}</span>
+        <p className="font-semibold lg:text-3xl">{name}</p>
+        <span className="lg:text-2xl">{description}</span>
       </div>
     </div>
   );
