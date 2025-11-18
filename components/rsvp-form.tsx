e6797a7eadee5570dd27520af090a90e100175aa
Mark Lewence Endrano <endranomarklewence@gmail.com>
2025-11-18 17:34:20 +0800

    feat(rsvp-form): integrated the onSubmit functionality

--- components/rsvp-form.tsx
+++ components/rsvp-form.tsx
@@ -1,8 +1,11 @@
 "use client";
 
 import { zodResolver } from "@hookform/resolvers/zod";
+import { Loader2, Send } from "lucide-react";
 import { useForm } from "react-hook-form";
-import { RSVPFormSchema, type TRSVPForm } from "@/config/api";
+import { toast } from "sonner";
+import { RSVPFormSchema, sendRSVPForm, type TRSVPForm } from "@/config/api";
+import { weddingCalendarURL } from "@/siteConfig";
 import { Button } from "./ui/button";
 import { Checkbox } from "./ui/checkbox";
 import {
@@ -22,14 +25,29 @@ export default function RSVPForm() {
   const form = useForm<TRSVPForm>({
     resolver: zodResolver(RSVPFormSchema),
     defaultValues: {
+      timestamp: Date.now(),
       isAttending: true,
       names: "",
       comments: "",
     },
   });
 
-  function onSubmit(data: TRSVPForm) {
-    console.log(data);
+  async function onSubmit(data: TRSVPForm) {
+    try {
+      await sendRSVPForm(data);
+
+      toast("🤵🏽 See you at the wedding! 👰🏼", {
+        action: {
+          label: "🗓️ Save the Date",
+          onClick: () => window.open(weddingCalendarURL, "_blank"),
+        },
+      });
+
+      form.reset();
+    } catch (error) {
+      toast.error(`${error}`);
+      console.error(error);
+    }
   }
 
   return (
@@ -52,8 +70,8 @@ export default function RSVPForm() {
                   <Checkbox
                     id="attendee"
                     defaultChecked
-                    onChange={field.onChange}
-                    required
+                    checked={field.value}
+                    onCheckedChange={field.onChange}
                     className="data-[state=checked]:bg-custom-primary data-[state=checked]:border-none"
                   />
                   <Label htmlFor="attendee" className="xl:text-xl">
@@ -76,19 +94,20 @@ export default function RSVPForm() {
               </FormLabel>
               <FormControl>
                 <Input
+                  {...field}
                   required
                   onChange={field.onChange}
                   className="text-dresscode-5 rounded-none bg-dresscode-4 border-none focus-visible:ring-custom-primary/50"
                 />
               </FormControl>
-              <FormMessage />
+              <FormMessage className="text-red-100" />
             </FormItem>
           )}
         />
 
         <FormField
           control={form.control}
-          name="names"
+          name="comments"
           render={({ field }) => (
             <FormItem>
               <FormLabel className="text-lg xl:text-xl font-semibold">
@@ -96,15 +115,17 @@ export default function RSVPForm() {
               </FormLabel>
               <FormControl>
                 <Textarea
+                  {...field}
+                  value={field.value || ""}
                   maxLength={100}
                   onChange={field.onChange}
                   className="text-dresscode-5 rounded-none bg-dresscode-4 border-none focus-visible:ring-custom-primary/50"
                 />
               </FormControl>
               <FormDescription className="text-white">
-                {field.value.length ?? 0} / 100 character
+                {field.value ? field.value.length : 0} / 100 character
               </FormDescription>
-              <FormMessage />
+              <FormMessage className="text-red-100" />
             </FormItem>
           )}
         />
@@ -112,8 +133,20 @@ export default function RSVPForm() {
         <Button
           variant="outline"
           className="w-full lg:w-fit bg-transparent font-inter rounded-sm"
+          disabled={
+            form.formState.isLoading ||
+            !form.formState.isDirty ||
+            form.formState.isSubmitting
+          }
         >
-          Submit
+          {form.formState.isSubmitting ? (
+            <>
+              Submitting
+              <Loader2 className="animate-spin" />
+            </>
+          ) : (
+            "Submit"
+          )}
         </Button>
       </form>
     </Form>
