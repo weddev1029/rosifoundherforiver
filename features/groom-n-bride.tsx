4f577e0677d4f015491e804dde1d4405c543601f
Mark Lewence Endrano <endranomarklewence@gmail.com>
2025-11-19 17:53:27 +0800

    refactor(groom-n-bride): added paragraphs as children for CoupleCard component

--- features/groom-n-bride.tsx
+++ features/groom-n-bride.tsx
@@ -8,13 +8,39 @@ export function GroomNBride() {
           url="/couple/groom.jpg"
           name="John Doe"
           description="The Groom"
-        />
+        >
+          <p>
+            Quiet but deeply thoughtful, Iverson is a man of few words but full
+            of love. He’s gentle, devoted, and guided by his faith. A lover of
+            music and mindful living, he finds peace in melodies and meaning in
+            taking care of both body and soul.
+          </p>
+
+          <p>
+            To her, he is home steady, sincere, and everything love was meant to
+            be.
+          </p>
+        </CoupleCard>
 
         <CoupleCard
           url="/couple/bride.jpg"
           name="Jane Smith"
           description="The Bride"
-        />
+          isGroom={false}
+        >
+          <p>
+            Rosilyn is a cheerful, compassionate soul whose smile lights up any
+            room. A K-drama lover, coffee enthusiast, and dog mama at heart, she
+            finds joy in life’s quiet moments and romantic twists.
+          </p>
+
+          <p>
+            Kind, warm, and full of love, she’s more than just a bride she’s his
+            sunshine, his calm, and his everyday joy. Now, she’s ready to begin
+            her own real-life love story one filled with laughter, loyalty, and
+            lots of love.
+          </p>
+        </CoupleCard>
       </div>
     </section>
   );
