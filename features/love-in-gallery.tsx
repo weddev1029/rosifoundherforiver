bcd788de34f8011a31820b273984ea868e13e1a1
Mark Lewence Endrano <endranomarklewence@gmail.com>
2025-11-20 17:59:15 +0800

    feat(love-in-gallery): added breathing animation

--- features/love-in-gallery.tsx
+++ features/love-in-gallery.tsx
@@ -1,9 +1,38 @@
+"use client";
+
+import { useGSAP } from "@gsap/react";
+import gsap from "gsap";
 import Image from "next/image";
+import { useRef } from "react";
 import ImageGallery from "@/components/image-gallery";
 
+gsap.registerPlugin(useGSAP); // register the hook to avoid React version discrepancies
+
 export function LoveInGallery() {
+  const container = useRef(null);
+
+  useGSAP(
+    () => {
+      gsap.to(".couple-image", {
+        keyframes: [
+          { scale: 0.95, duration: 1.5, ease: "power1.inOut" },
+          { scale: 1.0, duration: 1.5, ease: "power1.inOut" },
+        ],
+        repeat: -1,
+        stagger: {
+          each: 0.3,
+          from: "center",
+        },
+      });
+    },
+    { scope: container },
+  ); // <-- scope for selector text (optional)
+
   return (
-    <section className="p-8 lg:px-0 lg:flex bg-custom-secondary-3 justify-center items-center lg:py-10 xl:py-12">
+    <section
+      ref={container}
+      className="p-8 lg:px-0 lg:flex bg-custom-secondary-3 justify-center items-center lg:py-10 xl:py-12"
+    >
       <div className="grid grid-cols-2 lg:grid-cols-3">
         <div className="pb-8 lg:p-4 col-span-2 space-y-3 flex flex-col items-center lg:col-span-1 xl:p-8 md:justify-center">
           <div className="relative aspect-video min-w-[50px] max-w-[78.75px]">
@@ -22,16 +51,10 @@ export function LoveInGallery() {
           </h4>
         </div>
 
-        {/* {Array.from({ length: 5 }).map((_, index) => (
+        {Array.from({ length: 5 }).map((_, index) => (
           // biome-ignore lint/suspicious/noArrayIndexKey: <I have to>
           <ImageGallery key={index} />
-          ))} */}
-
-        <ImageGallery delay={2200} />
-        <ImageGallery delay={2400} />
-        <ImageGallery delay={2600} />
-        <ImageGallery delay={2800} />
-        <ImageGallery delay={3000} />
+        ))}
       </div>
     </section>
   );
